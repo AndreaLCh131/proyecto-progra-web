d485718c5372f41d9c30af338f5e52ac9e6c8cb0
dfs131294 <dfs131294@gmail.com>
2021-11-28 04:13:52 -0500

    avance cliente apuestas en vivo

--- src/app/layout/cliente/cliente.module.ts
+++ src/app/layout/cliente/cliente.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ClienteRoutingModule } from './cliente-routing.module';
 import { ClienteWrapperComponent } from './cliente-wrapper/cliente-wrapper.component';
 import { PartidaClienteComponent } from './partida-cliente/partida-cliente.component';
+import { HojaApuestasComponent } from './hoja-apuestas/hoja-apuestas.component';
 
 
 @NgModule({
   declarations: [
     PartidaClienteComponent,
-    ClienteWrapperComponent
+    ClienteWrapperComponent,
+    HojaApuestasComponent
   ],
   imports: [
     CommonModule,
